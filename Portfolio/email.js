1e0ef1e95e35c726c0fd5c579386fd3af4f59695
vtjaseem7 <vtjaseem7@gmail.com>
2025-07-03 13:41:51 +0530

    Add files via upload

---
+++ Portfolio/email.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_9qr5tak";
+  const templateID = "template_8jalp0t";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
